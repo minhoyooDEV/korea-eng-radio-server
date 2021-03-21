4cf0c1ff7a6e7ed84e766b8f64f772313aac9864
myno <minho.yoo.dev@gmail.com>
2021-03-21 18:18:47 +0900

    Add kakao login - get info

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -17,18 +17,50 @@ export class AuthController {
     console.log(body);
     return 'called post api';
   }
+
   @Get('kakao')
   @Header('content-type', 'text/html')
-  async kakaoRedirect(): Promise<any> {
-    return `<!DOCTYPE html>
-    asdasd
-        </html>`;
+  async kakaoRedirect(@Query('code') code): Promise<any> {
+    const result = await this.authService.getKakaoToken(code);
+    try {
+      const res = await result.toPromise();
+      console.log(res.data);
+      const token = (res.data as any).access_token;
+
+      const meResult = await this.authService.getMe(token);
+      const meRes = (await meResult.toPromise()).data;
+
+      return `<!DOCTYPE html>
+                ${code}
+                <div style="color: red"> 
+                ${JSON.stringify(res.data)}
+                <div>
+                <div style="color: blue"> 
+                ${JSON.stringify(meRes)}
+                <div>
+              </html>`;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
   }
+
+  @Get('kakao_service')
+  kakaoService() {
+    return `<!DOCTYPE html>errors</html>`;
+  }
+
   @Get('kakao_login')
   @Header('content-type', 'text/html')
   async kakaoAuth(): Promise<any> {
     return `<!DOCTYPE html>
+        <div>
         <a href=https://kauth.kakao.com/oauth/authorize?client_id=${'9622d0fd4d9984b34f0c1b0e94a07d48'}&redirect_uri=${'https://myno.loca.lt/auth/kakao'}&response_type=code>로그인하기 </a>
+        </div>
+        <div>
+        <a gref
+        </div>
+
         </html>`;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,44 @@
-// kakao app id: 367342
-
 import { HttpService, Injectable } from '@nestjs/common';
-
+export const KAKAO_CLIENT_ID = '9622d0fd4d9984b34f0c1b0e94a07d48';
 // default redirect to : https://myno.loca.lt/auth/kakao
 @Injectable()
 export class AuthService {
   constructor(private http: HttpService) {}
-  async getKakaoCode() {
-    try {
-      const result = await this.http.get(
-        `https://kauth.kakao.com/oauth/authorize?client_id=${367342}&redirect_uri=${'https://myno.loca.lt/auth/kakao'}&response_type=code`,
-      );
-      console.log('!!!!!!!!!!!');
-      return result.toPromise().then((r) => r);
-    } catch (error) {
-      console.log('@@@@@@@@@@@@@@');
-      console.error(error);
-      return error;
-    }
+  getKakaoToken(code: string) {
+    const params = new URLSearchParams();
+    params.append('grant_type', 'authorization_code');
+    params.append('client_id', `${KAKAO_CLIENT_ID}`);
+    params.append('redirect_uri', 'https://myno.loca.lt/auth/kakao');
+    params.append('code', code);
+
+    return this.http.post(`https://kauth.kakao.com/oauth/token`, params);
   }
+  getMe(token: string) {
+    return this.http.request({
+      url: 'https://kapi.kakao.com/v2/user/me',
+      headers: {
+        authorization: 'Bearer ' + token,
+      },
+    });
+  }
+  getKakaoScopes(scopes: string[]) {
+    return this.http.get(
+      `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${'https://myno.loca.lt/auth/kakao_service'}&response_type=code&scope=${scopes.join(
+        ',',
+      )}`,
+    );
+  }
+  // async getKakaoCode() {
+  //   try {
+  //     const result = await this.http.get(
+  //       `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${'https://myno.loca.lt/auth/kakao'}&response_type=code`,
+  //     );
+  //     console.log('!!!!!!!!!!!');
+  //     return result.toPromise().then((r) => r);
+  //   } catch (error) {
+  //     console.log('@@@@@@@@@@@@@@');
+  //     console.error(error);
+  //     return error;
+  //   }
+  // }
 }
